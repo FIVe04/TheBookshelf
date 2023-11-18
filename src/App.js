09429b4fc1227c496291b685e50c53525818d6b9
FIVe04 <ivan.a.frolov@gmail.com>
2023-11-18 20:31:10 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header'
+import Bestsellers from './components/Bestsellers/Bestsellers'
+import Search from './components/Search/Search'
+import {createBrowserRouter} from 'react-router-dom'
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Bestsellers />
+      <Search />
     </div>
   );
 }
